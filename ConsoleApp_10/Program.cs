e7a36aa3dcd4369dec7cd08953898689f5f18398
Samir Bagamaev <05ru4315@gmail.com>
2022-12-21 00:36:41 +0300

    ВОЗМОЖНОЕ РЕШЕНИЕ 10 ЗАДАЧИ

---
+++ ConsoleApp_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+
+Console.Write("Введи трёхзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(threeDigitNumber);
+
+
+Console.WriteLine("вторая цифра этого числа, выведенная после корректировки "+number[1]);
+
+
