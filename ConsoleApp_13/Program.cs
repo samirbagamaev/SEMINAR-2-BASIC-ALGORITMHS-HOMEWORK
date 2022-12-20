c6d0a14ef887a8495062616d06a04057f49b7d2a
Samir Bagamaev <05ru4315@gmail.com>
2022-12-21 00:38:42 +0300

    ВОЗМОЖНОЕ РЕШЕНИЕ 13 ЗАДАЧИ

---
+++ ConsoleApp_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// anythingNumber - некоторое число
+
+Console.WriteLine("Введите число");
+int anythingnumber = Convert.ToInt32(Console.ReadLine());
+string anythingNumber = Convert.ToString(anythingnumber);
+
+if (anythingNumber.Length > 2)
+{
+  Console.WriteLine("третья цифра заданного числа равняется " + anythingNumber[2]);
+}
+else
+{
+  Console.WriteLine("третья цифра заданного числа отсутсвует");
+}
